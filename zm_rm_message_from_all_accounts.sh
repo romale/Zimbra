1e71d15d3b83804548043d11edc02ea4bfe1c336
Alex R <aleromex@gmail.com>
2018-03-12 13:36:21 +0300

    Added: counter of moved messages, logging

--- zm_rm_message_from_all_accounts.sh
+++ zm_rm_message_from_all_accounts.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Removes message from all Zimbra accounts
 #
-# zm_rm_message.sh user@domain.com subject
+# zm_rm_message_from_all_accounts.sh user@domain.com "subject"
 # or
-# zm_rm_message.sh user@domain.com
+# zm_rm_message_from_all_accounts.sh user@domain.com
+
+LOG_POSTFIX=`strings /dev/urandom | tr -dc _A-Z-a-z-0-9 | head -c10`
+LOG_FILE="/tmp/zm_rm_message_from_all_accounts.$LOG_POSTFIX.log"
+COUNTER=0
 
 if [ -z "$2" ]; then
 addr=$1
@@ -11,10 +15,10 @@ for acct in `zmprov -l gaa | grep -E -v '(^admin@|^spam\..*@|^ham\..*@|^virus-qu
     echo "Searching $acct"
     for msg in `/opt/zimbra/bin/zmmailbox -z -m "$acct" s -l 999 -t message "from:$addr"|awk '{ if (NR!=1) {print}}' | grep -v -e Id -e "-" -e "^$" | awk '{ print $2 }'`
     do
-	#echo "Removing "$msg" from "$acct""
-	#/opt/zimbra/bin/zmmailbox -z -m $acct dm $msg
-	echo "Moving "$msg" from "$acct" to Trash"
-	/opt/zimbra/bin/zmmailbox -z -m $acct mm $msg /Trash
+        echo "Moving "$msg" from "$acct" to Trash" | tee >> $LOG_FILE
+        let COUNTER+=1
+        echo "Total found: $COUNTER"
+        /opt/zimbra/bin/zmmailbox -z -m $acct mm $msg /Trash
     done
 done
 else
@@ -24,11 +28,10 @@ for acct in `zmprov -l gaa | grep -E -v '(^admin@|^spam\..*@|^ham\..*@|^virus-qu
     echo "Searching $acct  for Subject:  $subject"
     for msg in `/opt/zimbra/bin/zmmailbox -z -m "$acct" s -l 999 -t message "from:$addr subject:$subject"|awk '{ if (NR!=1) {print}}' | grep -v -e Id -e "-" -e "^$" | awk '{ print $2 }'`
     do
-      #echo "Removing "$msg" from "$acct""
-      #/opt/zimbra/bin/zmmailbox -z -m $acct dm $msg
-      echo "Moving "$msg" from "$acct" to Trash"
-      /opt/zimbra/bin/zmmailbox -z -m $acct mm $msg /Trash
+        echo "Moving "$msg" from "$acct" to Trash" | tee >> $LOG_FILE
+        let COUNTER+=1
+        echo "Total found: $COUNTER"
+        /opt/zimbra/bin/zmmailbox -z -m $acct mm $msg /Trash
     done
 done
 fi
-
